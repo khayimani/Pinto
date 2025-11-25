72ab657b0c03102f42e811c905d9906bc665ad60
khayimani <khayimamiambo@gmail.com>
2025-11-26 01:06:07 +0200

    server update for google

--- src/server.js
+++ src/server.js
@@ -6,10 +6,12 @@ const unifiedRoutes = require('./routes/unified');
 
 app.use(express.json());
 
+// Mount the routers. All routes defined inside authRoutes will be prefixed with /auth
 app.use('/auth', authRoutes);
 app.use('/unified', unifiedRoutes);
 
 app.get('/', (req,res) => res.send('Pinto API Core is Active.'));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Pinto running on port ${PORT}'));
+// Using backticks for template literal interpolation
+app.listen(PORT, () => console.log(`Pinto running on port ${PORT}`));
